8708494a7f3f0b1e32d53541cd5182547dd2d8f7
bogdanov <bbogdan_ov@mail.ru>
2026-04-12 00:50:15 +0300

    chore: update SQL dump

--- dump.sql
+++ dump.sql
@@ -1,9 +1,9 @@
 /*M!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19-12.1.2-MariaDB, for Linux (x86_64)
+-- MariaDB dump 10.19-12.2.2-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: ohmysmal
 -- ------------------------------------------------------
--- Server version	12.1.2-MariaDB
+-- Server version	12.2.2-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,24 +28,14 @@ CREATE TABLE `comments` (
   `snippet_id` binary(16) NOT NULL,
   `author_id` int(10) unsigned NOT NULL,
   `text` varchar(300) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `author_id` (`author_id`),
-  KEY `snippet_id` (`snippet_id`),
-  CONSTRAINT `1` FOREIGN KEY (`snippet_id`) REFERENCES `snippets` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `2` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `key_comments_author_id` (`author_id`),
+  KEY `key_comments_snippet_id` (`snippet_id`),
+  CONSTRAINT `fk_comments_author_id` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_comments_snippet_id` FOREIGN KEY (`snippet_id`) REFERENCES `snippets` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `comments`
---
-
-LOCK TABLES `comments` WRITE;
-/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-set autocommit=0;
-/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
-UNLOCK TABLES;
-commit;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -58,7 +48,7 @@ DELIMITER ;;
 /*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER after_comment_insert
 AFTER INSERT ON comments
 FOR EACH ROW
-	UPDATE snippets SET comments=comments+1 WHERE id=NEW.snippet_id */;;
+        UPDATE snippets SET comments=comments+1 WHERE id=NEW.snippet_id */;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -94,23 +84,12 @@ CREATE TABLE `flowers` (
   `snippet_id` binary(16) NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`user_id`,`snippet_id`),
-  KEY `user_id` (`user_id`),
-  KEY `snippet_id` (`snippet_id`),
-  CONSTRAINT `1` FOREIGN KEY (`snippet_id`) REFERENCES `snippets` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+  KEY `key_flowers_user_id` (`user_id`),
+  KEY `key_flowers_snippet_id` (`snippet_id`),
+  CONSTRAINT `fk_flowers_snippet_id` FOREIGN KEY (`snippet_id`) REFERENCES `snippets` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_flowers_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `flowers`
---
-
-LOCK TABLES `flowers` WRITE;
-/*!40000 ALTER TABLE `flowers` DISABLE KEYS */;
-set autocommit=0;
-/*!40000 ALTER TABLE `flowers` ENABLE KEYS */;
-UNLOCK TABLES;
-commit;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -162,22 +141,15 @@ CREATE TABLE `snippets` (
   `flowers` int(10) unsigned NOT NULL DEFAULT 0,
   `comments` int(10) unsigned NOT NULL DEFAULT 0,
   `status` enum('ok','deleted') NOT NULL DEFAULT 'ok',
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `remix_of` binary(16) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `1` (`author_id`),
-  CONSTRAINT `1` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+  KEY `key_snippets_author_id` (`author_id`),
+  KEY `key_snippets_remix_of` (`remix_of`),
+  CONSTRAINT `fk_snippets_author_id` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_snippets_remix_of` FOREIGN KEY (`remix_of`) REFERENCES `snippets` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `snippets`
---
-
-LOCK TABLES `snippets` WRITE;
-/*!40000 ALTER TABLE `snippets` DISABLE KEYS */;
-set autocommit=0;
-/*!40000 ALTER TABLE `snippets` ENABLE KEYS */;
-UNLOCK TABLES;
-commit;
 
 --
 -- Temporary table structure for view `snippets_with_author`
@@ -194,6 +166,8 @@ SET character_set_client = utf8mb4;
   1 AS `flowers`,
   1 AS `comments`,
   1 AS `status`,
+  1 AS `date`,
+  1 AS `remix_of`,
   1 AS `author_nickname` */;
 SET character_set_client = @saved_cs_client;
 
@@ -210,22 +184,10 @@ CREATE TABLE `users` (
   `password` varchar(128) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   `role` enum('user','admin') NOT NULL DEFAULT 'user',
   `status` enum('ok','banned') NOT NULL DEFAULT 'ok',
+  `register_date` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-set autocommit=0;
-INSERT INTO `users` VALUES
-(18,'bogdanov','$2a$10$B/JZVskiBe/5A3A6lgbRPepXuSniTkWuERMYkARPgmPpw4Glhy7ui','admin','ok');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-commit;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -261,7 +223,8 @@ SET character_set_client = utf8mb4;
   1 AS `nickname`,
   1 AS `password`,
   1 AS `role`,
-  1 AS `status` */;
+  1 AS `status`,
+  1 AS `register_date` */;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -277,7 +240,7 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = utf8mb4_uca1400_ai_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `snippets_with_author` AS select `snippets`.`id` AS `id`,`snippets`.`author_id` AS `author_id`,`snippets`.`title` AS `title`,`snippets`.`flowers` AS `flowers`,`snippets`.`comments` AS `comments`,`snippets`.`status` + 0 AS `status`,`users`.`nickname` AS `author_nickname` from (`snippets` join `users` on(`snippets`.`author_id` = `users`.`id`)) */;
+/*!50001 VIEW `snippets_with_author` AS select `snippets`.`id` AS `id`,`snippets`.`author_id` AS `author_id`,`snippets`.`title` AS `title`,`snippets`.`flowers` AS `flowers`,`snippets`.`comments` AS `comments`,`snippets`.`status` + 0 AS `status`,`snippets`.`date` AS `date`,`snippets`.`remix_of` AS `remix_of`,`users`.`nickname` AS `author_nickname` from (`snippets` join `users` on(`snippets`.`author_id` = `users`.`id`)) */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -295,7 +258,7 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = utf8mb4_uca1400_ai_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `users_with_enums` AS select `users`.`id` AS `id`,`users`.`nickname` AS `nickname`,`users`.`password` AS `password`,`users`.`role` + 0 AS `role`,`users`.`status` + 0 AS `status` from `users` */;
+/*!50001 VIEW `users_with_enums` AS select `users`.`id` AS `id`,`users`.`nickname` AS `nickname`,`users`.`password` AS `password`,`users`.`role` + 0 AS `role`,`users`.`status` + 0 AS `status`,`users`.`register_date` AS `register_date` from `users` */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -309,4 +272,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2026-03-10 19:13:49
+-- Dump completed on 2026-04-12  0:49:45

dbcca61c7d59b53f2dcfb2090bbf8411bc7445e0
bogdanov <bbogdan_ov@mail.ru>
2026-04-15 01:40:33 +0300

    feat: bump the max file size limit

--- consts/consts.go
+++ consts/consts.go
@@ -3,7 +3,7 @@ package consts
 const (
 	MAX_PASSWORD_LEN      = 64 // max length of a password in runes (utf8 characters)
 	MAX_NICKNAME_LEN      = 32
-	MAX_SNIPPET_FILE_SIZE = 65535 // 64kb
+	MAX_SNIPPET_FILE_SIZE = 1024*1024 // 1mb
 	MAX_SNIPPET_TITLE_LEN = 500
 	MAX_COMMENT_TEXT_LEN  = 300
 )
